b1e01a42a25b26fbcebdd8e3f7b567a0cba4dc86
tomros766 <trosiek4@gmail.com>
2021-03-30 02:59:31 +0200

    test fixes

--- app/src/app/botlist/bot-details/bot-details.component.ts
+++ app/src/app/botlist/bot-details/bot-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-bot-details',
@@ -13,7 +13,7 @@ export class BotDetailsComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) {
     this.id = this.route.paramMap.pipe(
-      switchMap((params) => params.get('id')) //todo: gets last id char
+      switchMap((params) => params.get('id')), catchError(err => of("")) //todo: gets last id char
     )
         // switchMap() use to change observable type // todo after connection with backend
 
--- app/src/app/gamelist/game-details/game-details.component.ts
+++ app/src/app/gamelist/game-details/game-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-game-details',
   templateUrl: './game-details.component.html',
@@ -14,7 +14,7 @@ export class GameDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = this.route.paramMap.pipe(
-       switchMap((params) => params.get('id')) //todo: gets last id char
+      switchMap((params) => params.get('id')), catchError(err => of("")) //todo: gets last id char
     )
     // switchMap() use to change observable type // todo after connection with backend
   }
