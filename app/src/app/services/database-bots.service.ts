9bef440b9485385e98542fa0e5c237d2748af66d
Mateusz <mateuszobrzut628@gmail.com>
2021-05-04 23:25:40 +0200

    code refactor

--- app/src/app/services/database-bots.service.ts
+++ app/src/app/services/database-bots.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Bot } from '../objects/botInterface';
 import { BOTLIST } from '../objects/botList';
 
@@ -10,7 +11,7 @@ export class DatabaseBotsService {
   botsCollection: Bot[] = BOTLIST;
   SERVER_URL: string = "https://botcompetitionarena.herokuapp.com/upload-bot";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
   getBotList(context) {
     return this.botsCollection; //It will be much more sophisticated, promise
   }
@@ -32,6 +33,7 @@ export class DatabaseBotsService {
     const backendBot = {
       name: bot.name,
       version: bot.version,
+      queueId: bot.queueId,
       payload: bot.zip
     }
     console.log(backendBot)
@@ -46,6 +48,19 @@ export class DatabaseBotsService {
     
     var postRetrun = this.httpClient.post<any>(this.SERVER_URL, this.formData(backendBot), httpOptions).subscribe(res =>{
       console.log(res)
+      // switch (res.type) {
+      //   case HttpEventType.Sent:
+      //     console.log('Request has been made!');
+      //     break;
+      //   case HttpEventType.ResponseHeader:
+      //     console.log('Response header has been received!');
+      //     break;
+      //   case HttpEventType.Response:
+      //     console.log('Bot successfully created!', res.body);
+      //     alert("Bot successfully added!")
+      // }
+      // this.router.navigate(['/queue', bot.queueId])
+
     });
 
     // console.log(postRetrun)
